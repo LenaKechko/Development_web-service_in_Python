ebab8f1366fbba81db0d47b5a98f4156b78c8080
LenaKechko <ekechko@gmail.com>
2022-07-29 16:06:50 +0300

    Add files via upload

---
+++ Perceptron.py
@@ -0,0 +1,60 @@
+import numpy
+class Perceptron:
+
+    def __init__(self, w, b):
+        """
+        Инициализируем наш объект - перцептрон.
+        w - вектор весов размера (m, 1), где m - количество переменных
+        b - число
+        """
+
+        self.w = w
+        self.b = b
+
+    def forward_pass(self, single_input):
+        """
+        Метод рассчитывает ответ перцептрона при предъявлении одного примера
+        single_input - вектор примера размера (m, 1).
+        Метод возвращает число (0 или 1) или boolean (True/False)
+        """
+
+        result = 0
+        for i in range(0, len(self.w)):
+            result += self.w[i] * single_input[i]
+        result += self.b
+
+        if result > 0:
+            return 1
+        else:
+            return 0
+
+    def vectorized_forward_pass(self, input_matrix):
+        """
+        Метод рассчитывает ответ перцептрона при предъявлении набора примеров
+        input_matrix - матрица примеров размера (n, m), каждая строка - отдельный пример,
+        n - количество примеров, m - количество переменных
+        Возвращает вертикальный вектор размера (n, 1) с ответами перцептрона
+        (элементы вектора - boolean или целые числа (0 или 1))
+        """
+
+        y = []
+        for x in input_matrix:
+            y.append(self.forward_pass(x))
+        answer = numpy.array(y).reshape(len(input_matrix),1)
+        return answer
+
+    def train_on_single_example(self, example, y):
+        """
+        принимает вектор активации входов example формы (m, 1)
+        и правильный ответ для него (число 0 или 1 или boolean),
+        обновляет значения весов перцептрона в соответствии с этим примером
+        и возвращает размер ошибки, которая случилась на этом примере до изменения весов (0 или 1)
+        (на её основании мы потом построим интересный график)
+        """
+        res = self.forward_pass(example)
+        e = y - res
+        self.w += float(e) * example
+        self.b += float(e)
+        return e
+
+
